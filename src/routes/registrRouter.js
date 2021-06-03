137c4b6e2a380ce4d779540c7b62a8063d2a71f2
Kris <kristina.eldzarova@gmail.com>
2021-06-03 19:29:13 +0300

    merge ok

--- src/routes/registrRouter.js
+++ src/routes/registrRouter.js
@@ -1,31 +1,22 @@
-const { Router } = require("express");
-//const { route } = require('../app');
-const Ments = require("../db/models/ment.model");
+const { Router } = require('express');
+const Ments = require('../db/models/ment.model');
 
 const mentRouter = Router();
-const bcrypt = require("bcrypt");
-// ПАКЕТ ДЛЯ СОЗДАНИЯ ПАРОЛЯ
-const saltRound = 10; // ПО СТАНДАРТУ (подключения)
 
-
-mentRouter.get("/", (req, res) => {
-  res.render("signUp");
+mentRouter.get('/', (req, res) => {
+  res.render('signUp');
 });
 
-mentRouter.post("/", async (req, res) => {
-  
+mentRouter.post('/', async (req, res) => {
   const { name, email, password } = req.body;
   if (name && email && password) {
     const newMent = await Ments.create({ name, email, password });
-     if (newMent){
-      req.session.user = {id: newMent._id}
-      return res.redirect('/personalacc')
+    if (newMent) {
+      req.session.user = { id: newMent._id };
+      return res.redirect('/personalacc');
     }
-
- }
-  return res.status(418).redirect('/signUp')
+  }
+  return res.status(418).redirect('/signUp');
 });
-
-
 
 module.exports = mentRouter;
